db22f593ddd4d048e3c509f1dc68fffd2674099d
Ivo Delev <i.delev90@gmail.com>
2022-07-18 16:48:19 +0300

    installed tailwindcss react router dom and headles

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Outlet from "react-router-dom";
+
+function Layout() {
+    return (
+        <div className="w-screen h-screen dark:bg-black dark:text-white">
+            Layout
+        </div>
+    );
+}
+
+export default Layout;
