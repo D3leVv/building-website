c080629f466b9a255022201fd47951262fdee783
Ivo Delev <i.delev90@gmail.com>
2022-07-20 15:47:19 +0300

    added resize image library to useImageUpload hook

--- src/components/hooks/useImageUpload.tsx
+++ src/components/hooks/useImageUpload.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
     ref,
     uploadBytesResumable,
@@ -6,49 +6,76 @@ import {
     StorageError,
 } from "firebase/storage";
 import { storage } from "../../firebase/firebase-config";
+import Resizer from "react-image-file-resizer";
 
-const useFileUpload = (
+// function that resizes a file
+// https://github.com/onurzorluer/react-image-file-resizer#readme
+const resizeFile = (file: File) =>
+    new Promise((resolve) => {
+        Resizer.imageFileResizer(
+            file,
+            1240,
+            1240,
+            "JPEG",
+            100,
+            0,
+            (uri) => {
+                resolve(uri);
+            },
+            "file"
+        );
+    });
+function useFileUpload<T>(
     file: File | null,
     parentFolder = "news-images",
     childFolder: string
-) => {
-    const [progress, setProgress] = useState(0) as any;
+) {
+    const [progress, setProgress] = useState(0);
     const [error, setError] = useState<StorageError | null>(null);
-    const [url, setUrl] = useState() as any;
+    const [url, setUrl] = useState<{ url: string; alt: string }>();
 
     useEffect(() => {
-        if (!file) return;
-        //add file.name + file.exttention to the storageRef
-        const storageRef = ref(
-            storage,
-            `${parentFolder}/${childFolder}/${file.name}`
-        );
-        const uploadTask = uploadBytesResumable(storageRef, file);
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const prc = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-                setProgress(prc);
-            },
-            (error) => {
-                return setError(error);
-            },
-            () => {
-                getDownloadURL(storageRef)
-                    .then((url) => {
-                        setUrl({ url, name: file.name });
-                    })
-                    .catch((e) => {
-                        setError(e);
-                        console.log(e.message);
-                    });
-            }
-        );
+        async function runImage() {
+            if (!file) return;
+
+            const image: any = await resizeFile(file);
+
+            const storageRef = ref(
+                storage,
+                `${parentFolder}/${childFolder}/${image.name}`
+            );
+
+            const uploadTask = uploadBytesResumable(storageRef, image);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const prc = Math.round(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    );
+                    setProgress(prc);
+                },
+                (error) => {
+                    return setError(error);
+                },
+                () => {
+                    getDownloadURL(storageRef)
+                        .then((url) => {
+                            setUrl({
+                                url: url,
+                                alt: image.name,
+                            });
+                        })
+                        .catch((e) => {
+                            setError(e);
+                            console.log(e.message);
+                        });
+                }
+            );
+        }
+        runImage();
     }, [file, parentFolder, childFolder]);
 
     return { progress, error, url };
-};
+}
 
 export default useFileUpload;
