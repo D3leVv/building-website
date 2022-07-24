4d8987278f1cd0b140b9d48894bb51336ca08513
Ivo Delev <i.delev90@gmail.com>
2022-07-24 17:43:33 +0300

    added newsCard + connected to database

--- src/pages/News/index.tsx
+++ src/pages/News/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import NewsCard from "../../components/News/NewsCard/NewsCard";
 import { getMultipleDocsWithLimit } from "../../firebase/firebase-config";
+import { News as NewsType } from "../../Types/News";
 
 function News() {
-    getMultipleDocsWithLimit("news", 5);
+    const [news, setNews] = useState<NewsType[]>();
+    useEffect(() => {
+        (async () => {
+            try {
+                const limitedNews = await getMultipleDocsWithLimit("news", 5);
+                setNews(limitedNews);
+            } catch (e: any) {
+                setNews(e.message);
+            }
+        })();
+    }, []);
 
-    return <div>News</div>;
+    return (
+        <div className="container px-6 mx-auto mt-12">
+            {news && news.map((item, i) => <NewsCard {...item} key={i} />)}
+        </div>
+    );
 }
 
 export default News;
